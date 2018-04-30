58c3e365d28447cce6341af2bf88896489216d91
miicck <michael.hutcheon@hotmail.co.uk>
2018-04-30 20:29:50 +0100

    will now play chess against itself

--- game.js
+++ game.js
@@ -1,15 +1,31 @@
-// Namespace game
-var game = {
+var game = (function () { // Namespace game
 
-    // Start the game
-    start: function (playerName) {
-        console.log("Starting game");
-        console.log("Player: " + playerName);
+    // The chess board
+    var chess = new Chess();
 
-        var chess = new Chess();
+    // Redraw the board
+    function redrawBoard() {
         var main = document.getElementById("main");
         main.innerHTML = chess.ascii().replace(/(?:\r\n|\r|\n)/g, '<br>');
-        console.log(chess.ascii());
+    };
+
+    // One game tick
+    function tick() {
+        if (chess.game_over())
+            chess = new Chess();
+        var moves = chess.moves();
+        var move = moves[Math.floor(Math.random() * moves.length)];
+        chess.move(move);
+        redrawBoard();
+    };
+
+    // Public API
+    return {
+        start: function (playerName) {
+            console.log("Starting game");
+            console.log("Player: " + playerName);
+            setInterval(tick, 500);
+        }
     }
 
-} // End namespace game
+}()); // End namespace game
